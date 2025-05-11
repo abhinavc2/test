cf52484de12121cded26d676ae2c51e76c7e9941
abhinavc2 <abhinavreddychilumula@gmail.com>
2025-05-11 21:40:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/test/' 
+    
 
   plugins: [react()],
+   base: '/test/'
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
